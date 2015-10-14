945256ec4e01f350bca9e4e5e98cdc52aa901b8a
MatusT <matust@outlook.com>
2015-10-14 13:06:05 +0200

    repair by matus

--- KonJI/KonJI/main.c
+++ KonJI/KonJI/main.c
@@ -9,98 +9,9 @@
 #include "window.h"
 #include "entity.h"
 #include "sprite.h"
-<<<<<<< HEAD
-#include "lists.h"
-
-//int main()
-//{
-	//srand(time(0));
-=======
-#include "utilities.h"
->>>>>>> origin/master
-
-	//struct KonJIWindow* window = createKonJIWindow("KonJI", 80, 30);
-
-<<<<<<< HEAD
-	//clearConsoleBuffer(window);
-=======
-	struct Entity* entities;
-};
-
-
-int main()
-{
-	//srand(time(0));
-
-	struct KonJIWindow* window = createKonJIWindow("KonJI", 80, 30);
-
-	clearConsoleBuffer(window);
->>>>>>> origin/master
-	
-	/*struct Sprite sprite;
-	sprite.w = 3;
-	sprite.h = 2;
-	sprite.bitmap = malloc(6*sizeof(CHAR_INFO));
-
-	sprite.bitmap[0].Char.AsciiChar = 'A';
-	sprite.bitmap[0].Attributes = 100;
-
-	sprite.bitmap[1].Char.AsciiChar = 'B';
-	sprite.bitmap[1].Attributes = 71;
-
-	sprite.bitmap[2].Char.AsciiChar = 'C';
-	sprite.bitmap[2].Attributes = 58;
-
-	sprite.bitmap[3].Char.AsciiChar = 'D';
-	sprite.bitmap[3].Attributes = 200;
-
-	sprite.bitmap[4].Char.AsciiChar = 'E';
-	sprite.bitmap[4].Attributes = 143;
-
-	sprite.bitmap[5].Char.AsciiChar = 'F';
-	sprite.bitmap[5].Attributes = 43;
-	
-	writeSprite(&sprite, "data/test01.txt");
-	*/
-	//struct Sprite* sprite = loadSprite("data/churchill.txt");
-	//sprite->x = 15;
-	//sprite->y = 3;
-	//drawSprite(window, sprite, 15, 3);
-	//setPixel2c(window, 3, 5, 'A', 100);
-<<<<<<< HEAD
-	//while (1) {
-	//	drawKonJIWindow(window);
-	//}
-
-	//getchar();
-	/*struct Entity* e1 = malloc(sizeof(struct Entity));
-	e1->x = 1;
-
-	struct Entity* e2 = malloc(sizeof(struct Entity));
-	e2->x = 2;
-
-	struct Entity* e3 = malloc(sizeof(struct Entity));
-	e3->x = 3;
-
-
-	struct ListNode* l1 = create_ListNode_Entity(e1);
-
-	struct ListNode* l2 = create_ListNode_Entity(e2);
-
-	struct ListNode* l3 = create_ListNode_Entity(e3);
-
-	l1 = push_ListNode_Entity(l1, e3);
-	printf("%d %d\n", l1->entity->x, l1->next->entity->x);
-
-	l1 = swap_ListNode_Entity(l1, 0, 1);
-	printf("%d %d\n", l1->entity->x, l1->next->entity->x);
-
-//	l1 = insert_ListNode_Entity(l1, e2, 1);
-//	printf("%d %d %d\n", l1->entity->x, l1->next->entity->x, l1->next->next->entity->x);
-	*/
-//	scanf("");
-//}
 #include "palette.h"
+#include "lists.h"
+#include "utilities.h"
 
 /******************************************************************************
   main
@@ -127,90 +38,38 @@ int main(void)
 
   clearConsoleBuffer(window);
 
-  /*struct Sprite sprite;
-  sprite.w = 3;
-  sprite.h = 2;
-  sprite.bitmap = malloc(6*sizeof(CHAR_INFO));
-
-  sprite.bitmap[0].Char.AsciiChar = 'A';
-  sprite.bitmap[0].Attributes = 100;
-
-  sprite.bitmap[1].Char.AsciiChar = 'B';
-  sprite.bitmap[1].Attributes = 71;
-
-  sprite.bitmap[2].Char.AsciiChar = 'C';
-  sprite.bitmap[2].Attributes = 58;
-
-  sprite.bitmap[3].Char.AsciiChar = 'D';
-  sprite.bitmap[3].Attributes = 200;
-
-  sprite.bitmap[4].Char.AsciiChar = 'E';
-  sprite.bitmap[4].Attributes = 143;
-
-  sprite.bitmap[5].Char.AsciiChar = 'F';
-  sprite.bitmap[5].Attributes = 43;
-
-  writeSprite(&sprite, "data/test01.txt");
-  */
   struct Sprite* sprite = loadSprite("data/hitler_white.kgf");
-//  sprite->x = 15;
-  //sprite->y = 3;
+
   drawSprite(window, sprite, 15, 15);
- // setPixel2c(window, 3, 15, 'A', 100);
-  while (1) {
-  	drawKonJIWindow(window);
-  }
 
-  //getchar();
-  /*struct Entity* e1 = malloc(sizeof(struct Entity));
-  e1->x = 1;
-
-  struct Entity* e2 = malloc(sizeof(struct Entity));
-  e2->x = 2;
-
-  struct Entity* e3 = malloc(sizeof(struct Entity));
-  e3->x = 3;
-
-
-  struct ListNode* l1 = create_ListNode_Entity(e1);
-
-  struct ListNode* l2 = create_ListNode_Entity(e2);
-
-  struct ListNode* l3 = create_ListNode_Entity(e3);
-
-  l1 = push_ListNode_Entity(l1, e3);
-  printf("%d %d\n", l1->entity->x, l1->next->entity->x);
-
-  l1 = swap_ListNode_Entity(l1, 0, 1);
-  printf("%d %d\n", l1->entity->x, l1->next->entity->x);
-
-  //	l1 = insert_ListNode_Entity(l1, e2, 1);
-  //	printf("%d %d %d\n", l1->entity->x, l1->next->entity->x, l1->next->next->entity->x);
-  */
-  	scanf("");
- /* printf("Small default font.\n");
-  Sleep(2000);
-  
-  SetConsolePalette(palette, 1, 2, L"Lucida Console");
-  printf("Too tiny to read Lucida!\n");
-  Sleep(2000);
-  
-  if (windowsVersionTest())
+  double previous = getCurrentTime();
+  double lag = 0.0;
+  while (1)
   {
-    SetConsolePalette(palette, 8, 12, L"Consolas");
-    printf("More of a normal Consolas size.\n");
+	  double current = getCurrentTime();
+	  double elapsed = current - previous;
+	  previous = current;
+	  lag += elapsed;
+
+	  // process input
+	  if (_kbhit()) {
+		  char c = _getch();
+
+		  if (c == 'x') {
+			  break;
+		  }
+	  }
+
+	  while (lag >= 16.666666666666668)
+	  {
+		  //update();
+		  lag -= 16.666666666666668;
+	  }
+
+	  drawKonJIWindow(window);
   }
-  else
-  {
-    SetConsolePalette(palette, 8, 12, L"Lucidas");
-    printf("More of a normal Lucidas size.\n");
-  }
-  Sleep(2000);
   
-  SetConsolePalette(palette, 43, 72, L"Lucida Console");
-  printf("Huge Lucida!\n");
-  
-  getchar();*/
+  scanf("");
 
   return 0;
 }
@@ -331,35 +190,6 @@ VOID WINAPI SetConsolePalette(COLORREF palette[], int fontX, int fontY, wchar_t 
 
 		SetConsoleInfo(hwndConsole, &ci);
 	}
-=======
-
-	double previous = getCurrentTime();
-	double lag = 0.0;
-	while (1)
-	{
-		double current = getCurrentTime();
-		double elapsed = current - previous;
-		previous = current;
-		lag += elapsed;
-
-		// process input
-		if (_kbhit()) {
-			char c = _getch();
-
-			if (c == 'x') {
-				break;
-			}
-		}
-
-		while (lag >= 16.666666666666668)
-		{
-			//update();
-			lag -= 16.666666666666668;
-		}
-
-		drawKonJIWindow(window);
-	}
->>>>>>> origin/master
 }
 
 /******************************************************************************
