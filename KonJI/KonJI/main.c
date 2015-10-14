3104362bc0bdbed99f0140f8d6881430c1ad1520
MatusT <matust@outlook.com>
2015-10-14 13:52:30 +0200

    clean main

--- KonJI/KonJI/main.c
+++ KonJI/KonJI/main.c
@@ -13,29 +13,21 @@
 #include "lists.h"
 #include "utilities.h"
 
+const COLORREF palette[16] =
+{
+	0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
+	0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
+	0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
+	0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00000000
+};
+
 int main(void)
 {
-    /* Setting the color palette to the default colors
-    /* Browse MSDN for COLORREF to learn more about these RGB values */
-  COLORREF palette[16] = 
-  {
-	  0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
-	  0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
-	  0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00ffffff,
-	  0x00ffffff, 0x00ffffff, 0x00ffffff, 0x00000000
-  };
-    /* Search MSDN for the RGB macro to easily generate COLORREF values */
 
   SetConsolePalette(palette, 8, 8, L"");
-  srand(time(0));
 
   struct KonJIWindow* window = createKonJIWindow("KonJI", 80, 50);
-
-  clearConsoleBuffer(window);
-
   struct Sprite* sprite = loadSprite("data/hitler_white.kgf");
-
-  drawSprite(window, sprite, 15, 15);
 
   double previous = getCurrentTime();
   double lag = 0.0;
@@ -52,6 +44,8 @@ int main(void)
 		  lag -= 16.666666666666668;
 	  }
 
+	  clearConsoleBuffer(window);
+	  drawSprite(window, sprite, 15, 15);
 	  drawKonJIWindow(window);
   }
 
